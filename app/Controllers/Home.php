286cf1d132ab091cedd5bfc2a9312b5ca31edf9e
Ferdiansyah Ramadhan <44073700+ferdi1ansyah@users.noreply.github.com>
2021-03-22 07:35:37 +0700

    project masih login

---
+++ app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('auth/login');
+	}
+
+	public function register()
+	{
+		return view('auth/registep');
+	}
+}

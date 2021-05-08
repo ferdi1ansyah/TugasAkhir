992ff21feb022909d68fe0e889e80f0957e6e0e1
Dwi Nur Cahyo <47982352+dwinuray@users.noreply.github.com>
2021-05-08 10:18:45 +0700

    Tampilan Data Materi

--- app/Controllers/DataMateri.php
+++ app/Controllers/DataMateri.php
@@ -2,10 +2,30 @@
 namespace App\Controllers;
 use CodeIgniter\Controller;
 
-class DataMateri extends Controller {
+class Datamateri extends Controller {
+
+
+    private $session;
+
+    function __construct(){
+        
+        $this->session = \Config\Services::session();   
+
+        // cek sesi aktif 
+        if ( empty( $this->session->get('sess_username') ) ) {
+
+            // flashdata
+            $this->session->setFlashdata('pesan', 'Harap login terlebih dahulu');
+
+            $url = base_url('login/index');
+            header('Location: '.$url);
+
+            exit(); 
+        }
+    }
 
     function index () {
 
-        return view("guru/datamateri/V_datamateri.php");
+        return view("guru/datamateri/V_datamateri");
     }
 }

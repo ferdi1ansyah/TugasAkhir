68bd72e5e321e20b8dce5ba8cad4756e3d312f06
Dwi Nur Cahyo <47982352+dwinuray@users.noreply.github.com>
2021-06-10 23:19:29 +0700

    Perbaikkan halaman user

--- app/Views/templates/template_login_new.php
+++ app/Views/templates/template_login_new.php
@@ -70,7 +70,7 @@ License: You must have a valid license purchased only from themeforest(the above
 						<!--begin::Signin-->
 						<div class="login-form login-signin">
 							<!--begin::Form-->
-							<form class="form" novalidate="novalidate" id="kt_login_signin_form">
+							<form action="<?php echo base_url('login/proseslogin') ?>" class="form" novalidate="novalidate" id="" method="POST">
 								<!--begin::Title-->
 								<div class="pb-13 pt-lg-0 pt-5">
 									<h3 class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg">Welcome To Pipel Academy</h3>
@@ -95,7 +95,7 @@ License: You must have a valid license purchased only from themeforest(the above
 								<!--end::Form group-->
 								<!--begin::Action-->
 								<div class="pb-lg-0 pb-5">
-									<button type="button" id="kt_login_select_submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Log in</button>
+									<button type="submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Log in</button>
 									<button type="button" class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-lg">
 									<span class="svg-icon svg-icon-md">
 										<!--begin::Svg Icon | path:assets/media/svg/social-icons/google.svg-->
@@ -221,12 +221,12 @@ License: You must have a valid license purchased only from themeforest(the above
 		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#1BC5BD", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#6993FF", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#1BC5BD", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#E1E9FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
 		<!--end::Global Config-->
 		<!--begin::Global Theme Bundle(used by all pages)-->
-		<script src="<?php echo base_url() ?>/assets/plugins/global/plugins.bundle.js"></script>
-		<script src="<?php echo base_url() ?>/assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
-		<script src="<?php echo base_url() ?>/assets/js/scripts.bundle.js"></script>
+		<script src="<?php echo base_url() ?>/assets/metro/plugins/global/plugins.bundle.js"></script>
+		<script src="<?php echo base_url() ?>/assets/metro/plugins/custom/prismjs/prismjs.bundle.js"></script>
+		<script src="<?php echo base_url() ?>/assets/metro/js/scripts.bundle.js"></script>
 		<!--end::Global Theme Bundle-->
 		<!--begin::Page Scripts(used by this page)-->
-		<script src="<?php echo base_url() ?>/assets/js/pages/custom/login/login-general.js"></script>
+		<script src="<?php echo base_url() ?>/assets/metro/js/pages/custom/login/login-general.js"></script>
 		<!--end::Page Scripts-->
 	</body>
 	<!--end::Body-->

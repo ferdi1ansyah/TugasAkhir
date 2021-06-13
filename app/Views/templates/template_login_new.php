3a1db77b54cbe696a1bce2ccd56752bff57c7adf
Ferdiansyah Ramadhan <44073700+ferdi1ansyah@users.noreply.github.com>
2021-06-13 15:37:08 +0700

    nambah logo ndek login

--- app/Views/templates/template_login_new.php
+++ app/Views/templates/template_login_new.php
@@ -38,7 +38,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--end::Global Theme Styles-->
 		<!--begin::Layout Themes(used by all pages)-->
 		<!--end::Layout Themes-->
-		<link rel="shortcut icon" href="<?php echo base_url() ?>/assets/metro/media/logos/favicon.ico" />
+		<link rel="shortcut icon" href="<?php echo base_url() ?>/assets/metro/media/logos/logo-primary" />
 	</head>
 
 

f7c4d2423a6ba44e5a3aaadb93ed050dd4b108e4
Ferdiansyah Ramadhan <44073700+ferdi1ansyah@users.noreply.github.com>
2021-06-14 06:16:12 +0700

    benerin template guru onboarding

--- app/Views/templates/template_landingGuru.php
+++ app/Views/templates/template_landingGuru.php
@@ -12,12 +12,12 @@
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/saaspal/css/bootstrap-5.0.0-alpha-2.min.css" />
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/saaspal/css/LineIcons.2.0.css" />
+    <link rel="stylesheet" href="<?php echo base_url() ?>/assets/saaspal/css/bootstrap-5.0.0-alpha-2.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url() ?>/assets/saaspal/css/LineIcons.2.0.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/saaspal/css/tiny-slider.css" />
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/saaspal/css/animate.css" />
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/saaspal/css/main.css" />
+    <link rel="stylesheet" href="<?php echo base_url() ?>/assets/saaspal/css/tiny-slider.css" />
+    <link rel="stylesheet" href="<?php echo base_url() ?>/assets/saaspal/css/animate.css" />
+    <link rel="stylesheet" href="<?php echo base_url() ?>/assets/saaspal/css/main.css" />
 
 </head>
 
@@ -66,7 +66,7 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="hero-img wow fadeInUp" data-wow-delay=".5s">
-                        <img src="assets/img/hero/hero-img2.png" alt="">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/hero/hero-img2.png" alt="">
                     </div>
                 </div>
             </div>
@@ -79,61 +79,8 @@
             <div class="row align-items-center">
                 <div class="col-xl-6 col-lg-6">
                     <div class="about-img">
-                        <img src="assets/img/about/about-1.png" alt="" class="w-100">
-                        <img src="assets/img/about/about-left-shape.svg" alt="" class="shape shape-1">
-                        <img src="assets/img/about/left-dots.svg" alt="" class="shape shape-2">
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-6">
-                    <div class="about-content">
-                        <div class="section-title mb-30">
-                            <h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">Perfect Solution Thriving Online Business</h1>
-                            <p class="wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed dinonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem.Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <a href="javascript:void(0)" class="main-btn btn-hover border-btn wow fadeInUp" data-wow-delay=".6s">Discover More</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section id="about" class="about-section pt-150">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-xl-6 col-lg-6">
-                    <div class="about-content">
-                        <div class="section-title mb-30">
-                            <h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">Easy to Use with Tons of Awesome Features</h1>
-                            <p class="wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-                        </div>
-                        <ul>
-                            <li>Quick Access</li>
-                            <li>Easily to Manage</li>
-                            <li>24/7 Support</li>
-                        </ul>
-                        <a href="javascript:void(0)" class="main-btn btn-hover border-btn wow fadeInUp" data-wow-delay=".6s">Learn More</a>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-6 order-first order-lg-last">
-                    <div class="about-img-2">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/about-2.png" alt="" class="w-100">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/about-right-shape.svg" alt="" class="shape shape-1">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/right-dots.svg" alt="" class="shape shape-2">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section id="about" class="about-section pt-150">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-xl-6 col-lg-6">
-                    <div class="about-img">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/about-1.png" alt="" class="w-100">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/about-left-shape.svg" alt="" class="shape shape-1">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-1.png" alt="" class="w-100">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-left-shape.svg" alt="" class="shape shape-1">
                         <img src="<?php echo base_url() ?>assets/saaspal/img/about/left-dots.svg" alt="" class="shape shape-2">
                     </div>
                 </div>
@@ -170,9 +117,62 @@
                 </div>
                 <div class="col-xl-6 col-lg-6 order-first order-lg-last">
                     <div class="about-img-2">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/about-2.png" alt="" class="w-100">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/about-right-shape.svg" alt="" class="shape shape-1">
-                        <img src="<?php echo base_url() ?>assets/saaspal/img/about/right-dots.svg" alt="" class="shape shape-2">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-2.png" alt="" class="w-100">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-right-shape.svg" alt="" class="shape shape-1">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/right-dots.svg" alt="" class="shape shape-2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="about" class="about-section pt-150">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="about-img">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-1.png" alt="" class="w-100">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-left-shape.svg" alt="" class="shape shape-1">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/left-dots.svg" alt="" class="shape shape-2">
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="about-content">
+                        <div class="section-title mb-30">
+                            <h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">Perfect Solution Thriving Online Business</h1>
+                            <p class="wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed dinonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem.Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <a href="javascript:void(0)" class="main-btn btn-hover border-btn wow fadeInUp" data-wow-delay=".6s">Discover More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="about" class="about-section pt-150">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="about-content">
+                        <div class="section-title mb-30">
+                            <h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">Easy to Use with Tons of Awesome Features</h1>
+                            <p class="wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                        </div>
+                        <ul>
+                            <li>Quick Access</li>
+                            <li>Easily to Manage</li>
+                            <li>24/7 Support</li>
+                        </ul>
+                        <a href="javascript:void(0)" class="main-btn btn-hover border-btn wow fadeInUp" data-wow-delay=".6s">Learn More</a>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 order-first order-lg-last">
+                    <div class="about-img-2">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-2.png" alt="" class="w-100">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-right-shape.svg" alt="" class="shape shape-1">
+                        <img src="<?php echo base_url() ?>/assets/saaspal/img/about/right-dots.svg" alt="" class="shape shape-2">
                     </div>
                 </div>
             </div>
@@ -192,14 +192,14 @@
                 <div class="row align-items-center">
                     <div class="col-xl-6 col-lg-7">
                         <div class="section-title mb-15">
-                            <h1 class="text-white mb-25">Ayo Daftar Sekarang</h1>
-                            <p class="text-white pr-5">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <h1 class="text-white mb-25">Silahkan Download Materi Aksesbilitas</h1>
+                            <p class="text-white pr-5">Download Sumber Belajar materi materi yang saya dapatkan dari expert dan praktiksi</p>
                         </div>
                     </div>
                     <div class="col-xl-6 col-lg-5">
                         <form action="#" class="subscribe-form">
 
-                            <button type="submit" class="btn-light" texr="main">Mulai Daftar</button>
+                            <button type="submit" class="btn-light" texr="main">Download</button>
                         </form>
                     </div>
                 </div>
@@ -277,10 +277,10 @@
         <i class="lni lni-chevron-up"></i>
     </a>
 
-    <script src="<?php echo base_url() ?>assets/saaspal/js/bootstrap.5.0.0.alpha-2-min.js"></script>
-    <script src="<?php echo base_url() ?>assets/saaspal/js/tiny-slider.js"></script>
-    <script src="<?php echo base_url() ?>assets/saaspal/js/wow.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/saaspal/js/main.js"></script>
+    <script src="<?php echo base_url() ?>/assets/saaspal/js/bootstrap.5.0.0.alpha-2-min.js"></script>
+    <script src="<?php echo base_url() ?>/assets/saaspal/js/tiny-slider.js"></script>
+    <script src="<?php echo base_url() ?>/assets/saaspal/js/wow.min.js"></script>
+    <script src="<?php echo base_url() ?>/assets/saaspal/js/main.js"></script>
 </body>
 
 <!-- Mirrored from preview.uideck.com/items/saaspal/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 10 May 2021 04:21:37 GMT -->

34df956e6756a716b84e50704061e9ee1e768da5
Ferdiansyah Ramadhan <44073700+ferdi1ansyah@users.noreply.github.com>
2021-06-16 04:45:56 +0700

    del

--- app/Views/templates/template_landingpage_utama.php
+++ app/Views/templates/template_landingpage_utama.php
@@ -212,30 +212,6 @@ Layanan Pembelajaran bagi seluruh orang yang ingin belajar dengan tanpa kendala 
 </section>
 
 
-<section id="about" class="about-section pt-150">
-<div class="container">
-<div class="row align-items-center">
-<div class="col-xl-6 col-lg-6">
-<div class="about-img">
-<img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-3" alt="" class="w-100">
-<img src="<?php echo base_url() ?>/assets/saaspal/img/about/about-left-shape.svg" alt="" class="shape shape-1">
-<img src="<?php echo base_url() ?>/assets/saaspal/img/about/left-dots.svg" alt="" class="shape shape-2">
-</div>
-</div>
-<div class="col-xl-6 col-lg-6">
-<div class="about-content">
-<div class="section-title mb-30">
-<h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">Buat Konten Menarik untuk diajarkan</h1>
-<p class="wow fadeInUp" data-wow-delay=".4s">buat konten yang menarik untuk diajarkan sebagai pemula.</p>
-</div>
-<a href="javascript:void(0)" class="main-btn btn-hover border-btn wow fadeInUp" data-wow-delay=".6s">Discover More</a>
-</div>
-</div>
-</div>
-</div>
-</section>
-
-
 
 
 
